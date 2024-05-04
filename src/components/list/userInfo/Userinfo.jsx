9f8445fbae1100bf04ccf78351ced6ab7e543131
nitesh-20 <nsahu6156@gmail.com>
2024-05-04 09:10:56 +0530

    chatList-section

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 
 const Userinfo = () => {
   return (
-    <div>userinfo</div>
+    <div className="userInfo">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>NITESH</h2>
+      </div>
+      <div className="icons">
+        <img src="./more.png" alt="" />
+        <img src="./video.png" alt="" />
+        <img src="./edit.png" alt="" />
+      </div>
+    </div>
   )
 }
 
